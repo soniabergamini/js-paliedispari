449814f91f884430ab54e89c3e39b2d625c8f5fa
soniabergamini <sounia96@outlook.it>
2023-04-21 15:21:01 +0200

    feat(js): add palindrome word function

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,1 +1,24 @@
-prompt("Ciao");
+// FUNZIONE PAROLE PALINDROME
+function isPal() {
+
+    for (let i = word.length - 1; i >= 0; i--) {
+        wordReverse += word[i];
+        // console.log("lettera per lettera al contrario:", wordReverse)
+    }
+    
+    console.log("La parola al contrario è:", wordReverse)
+
+    if (wordReverse === word) {
+        // console.log(`${word} è palindroma!`);
+        return true;
+    } 
+    // console.log(`${word} NON è palindroma!`);
+    return false;
+}
+
+// Dichiaro Variabili
+let word = prompt("Inserisci una parola");
+console.log("L'utente scrive: ", word);
+let wordReverse = "";
+let risultato = isPal(word);
+console.log("La parola è palindroma? La funzione dice:", risultato)
