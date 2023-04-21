b985593ccb58a066788325e1e476248ef412c532
soniabergamini <sounia96@outlook.it>
2023-04-21 15:24:51 +0200

    refactor(js): add comment

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,6 +1,7 @@
-// FUNZIONE PAROLE PALINDROME
+// FUNZIONE PAROLA PALINDROMA
 function isPal() {
 
+    // ciclo for che scrive la parola al contrario
     for (let i = word.length - 1; i >= 0; i--) {
         wordReverse += word[i];
         // console.log("lettera per lettera al contrario:", wordReverse)
@@ -8,6 +9,7 @@ function isPal() {
     
     console.log("La parola al contrario è:", wordReverse)
 
+    // if controlla se la parola al contrario è uguale alla parola scritta dall'utente e la funzione restituisce true/false.
     if (wordReverse === word) {
         // console.log(`${word} è palindroma!`);
         return true;
@@ -16,7 +18,7 @@ function isPal() {
     return false;
 }
 
-// Dichiaro Variabili
+// DICHIARO VARIABILI
 let word = prompt("Inserisci una parola");
 console.log("L'utente scrive: ", word);
 let wordReverse = "";
